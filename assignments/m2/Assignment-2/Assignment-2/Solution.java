1a8298f34ffea7b4fc1311ffc59e8a163ce11b7d
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-28 18:30:37 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -3,35 +3,35 @@ import java.lang.Math;
 /**.
  * This program is for printing quadratic equation.
  */
-public class Solution {
-	private Solution() {
-		/**.
-		 * this is constuctor
-		 */
-	}
-	/**.
-	 * This is a function for printing quadratic equation
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**.
-	 * This is a function for printing quadratic equation
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 */
-	public static void rootsOfQuadraticEquation(int a,int b,int c) {
-		long d = (b * b) - (4 * a * c);
-		double root1 = (-b + (Math.sqrt(Math.abs(d)))) / (2 * a);
-		double root2 = (-b - (Math.sqrt(Math.abs(d)))) / (2 * a);
-		System.out.println(root1 + " " + root2);
-	}
+final public class Solution {
+    private Solution() {
+        /**.
+         * this is constuctor
+         */
+    }
+    /**.
+     * This is a function for printing quadratic equation
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        equation(a, b, c);
+    }
+    /**.
+     * This is a function for printing quadratic equation
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    public static void equation(final int a,final int b,final int c) {
+        long d = (b * b) - (4 * a * c);
+        double root1 = (-b + (Math.sqrt(Math.abs(d)))) / (2 * a);
+        double root2 = (-b - (Math.sqrt(Math.abs(d)))) / (2 * a);
+        System.out.println(root1 + " " + root2);
+    }
 }
