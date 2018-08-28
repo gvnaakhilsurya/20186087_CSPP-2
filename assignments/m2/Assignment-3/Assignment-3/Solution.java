55b8cb9744c8591d897fe8f6263a354af02cb50d
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-28 18:42:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m2/Assignment-3/Assignment-3/Solution.java
+++ assignments/m2/Assignment-3/Assignment-3/Solution.java
@@ -1,36 +1,47 @@
 import java.util.Scanner;
-
-public class Solution {
-    /**
-     * It defines the main function that deals the. 
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * It defines the main function that deals the.
      *
      * @param      base      its's the int value
      * @param      exponent  it's the exponent value
-     *
      * @return     it returns the value
      */
-    public static long power(int base,int exponent){
-       
+    public static long power(final int base, final int exponent) {
         {
             if (exponent > 0)
-                return  base * power(base,exponent-1);
+                return  base * power(base, exponent - 1);
             return 1;
-            
         }
-
     }
-    
+    /**.
+     * It defines the main function.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
-        /**
+        /**.
          * { var_description }
          */
         Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result = power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
     }
     /*
     Need to write the power function and print the output.
     */
 }
+
