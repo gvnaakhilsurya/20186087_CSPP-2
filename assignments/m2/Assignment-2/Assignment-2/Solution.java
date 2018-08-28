abb5e64255a49bfeb0c45d0d91a6c3378e44fde3
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-28 17:19:10 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -17,8 +17,8 @@ public class Solution {
 	*/
 	public static void rootsOfQuadraticEquation(int a,int b,int c){
 		long d = (b * b) - (4 * a * c);
-		double root1 = (-b + Math.sqrt(d)) / (2 * a);
-		double root2 = (-b + Math.sqrt(d)) / (2 * a);
+		double root1 = (-b + (Math.sqrt(d))) / (2 * a);
+		double root2 = (-b + (Math.sqrt(d))) / (2 * a);
 		System.out.println(root1 + " " + root2);
 	}
 }
