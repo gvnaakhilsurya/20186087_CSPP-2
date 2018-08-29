31e67022364f1460af68abc57cbb45ef95ab47a7
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-29 18:40:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m3/Assignment-2/Assignment-2/Solution.java
+++ assignments/m3/Assignment-2/Assignment-2/Solution.java
@@ -1,30 +1,47 @@
-
+/**
+ * @author: akhil.
+ */
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
-public class Solution {
-	public static void counter(int n){
-		int count = 0;
-		for (int i=1 ;i <n ;i++){
-			int j = i;
-			while(j!=0){
-				int c = j%10;
-				j = j/10;
-				if (c ==7 ){
-					count = count +1;
-		        }
-            
+/**
+* Do not modify this main function.
+*/
+public final class Solution {
+/**
+* This is yhe main class.
+*/
+    private Solution() {
+/**
+* it is a countructor.
+*/
+    }
+/**
+* @param      n     int.
+*/
+    public static void counter(final int n) {
+/**
+* it is the method.
+*/
+        int count = 0;
+        final int xVAR = 7;
+        final int yVAR = 10;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j != 0) {
+                int c = j % yVAR;
+                j = j / yVAR;
+                if (c == xVAR) {
+                    count = count + 1;
+                }
             }
-				
-		}
-		System.out.println(count);
-	}
-    public static void main(String[] args) {
-
-        Scanner s=new Scanner(System.in);      
+    }
+        System.out.println(count);
+    }
+    /**
+     * @param args are of string type.
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         counter(n);
-    
-	}
+    }
 }
