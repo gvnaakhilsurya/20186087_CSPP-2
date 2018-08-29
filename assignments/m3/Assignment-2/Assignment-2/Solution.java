4497115145cf998d955172a839e4754c798602b1
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-29 17:35:33 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ assignments/m3/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+/*
+	Do not modify this main function.
+	*/
+public class Solution {
+	public static void counter(int x){
+		int count = 0;
+		for (int i=1 ;i <x ;i++){
+			int j = i;
+			while(j!=0){
+				int c = j%10;
+				j = j/10;
+				if (c ==7 ){
+					count = count +1;
+				}System.out.println(count);
+		}	} 
+	}
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);      
+        int n = s.nextInt(); 
+    
+	}
+}
