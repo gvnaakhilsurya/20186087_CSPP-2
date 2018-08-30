f091dde7c353605c4bdb03fdbcadfc732f7f7565
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-30 23:18:13 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m4/Assignment-1/Assignment-1/Solution.java
+++ assignments/m4/Assignment-1/Assignment-1/Solution.java
@@ -13,9 +13,7 @@ class Solution{
 
  	public static void main(String[] args) {
  		Scanner sc = new Scanner(System.in);
- 		System.out.println("Enter the size of an array:");
  		int n = sc.nextInt();
-		System.out.println("Enter the array Elements");
  		int a[] = new int[n];
  		for (int i = 0;i<n;i++){
  			a[i]= sc.nextInt();
