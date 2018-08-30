43b12a47fa2b04f7f4cac332b4d81c265d92f53c
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-30 23:22:29 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m4/Assignment-1/Assignment-1/Solution.java
+++ assignments/m4/Assignment-1/Assignment-1/Solution.java
@@ -1,6 +1,16 @@
 import java.util.*;
 class Solution{
+	/**.
+	 * It is the definition of main class.
+	 *
+	 * @param      a     array
+	 *
+	 * @return     int
+	 */
  	public static int largenum(int[] a){
+ 		/**.
+ 		 * It is the method of the main class.
+ 		 */
  		int max;
  		max = a[0];
  		for (int i : a){
@@ -12,11 +22,14 @@ class Solution{
  	}
 
  	public static void main(String[] args) {
+ 		/**.
+ 		 * It is main class.
+ 		 */
  		Scanner sc = new Scanner(System.in);
  		int n = sc.nextInt();
  		int a[] = new int[n];
  		for (int i = 0;i<n;i++){
- 			a[i]= sc.nextInt();
+ 			a[i] = sc.nextInt();
  		 }
  		 System.out.println(largenum(a));
  		 
