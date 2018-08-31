cf5945b419f7fafa87fa7a5c4b5bf4d3ac4bfa5e
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-31 14:15:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m4/Assignment-4/Assignment-4/Solution.java
+++ assignments/m4/Assignment-4/Assignment-4/Solution.java
@@ -6,37 +6,35 @@ import java.util.*;
  * Class for solution.
  */
 class Solution {
-	/**
-	 * It is used to reverse the string.
-	 *
-	 * @param      s    string
-	 *
-	 * @return     string
-	 */
-	public static String reverse(final String s) {
-		/**
-		 * x = empty string
-		 * s = string input to yhe function.
-		 */
-		String x = "";
-		for(int i = 0; i<s.length() ;i++) {
-			x = s.charAt(i) + x;
-		}return x;
+    /**
+     * It is used to reverse the string.
+     *
+     * @param      s    string
+     *
+     * @return     string
+     */
+    public static String reverse(final String s) {
+        /**
+         * x = empty string
+         * s = string input to yhe function.
+         */
+        String x = "";
+        for(int i = 0; i<s.length() ;i++) {
+            x = s.charAt(i) + x;
+        }return x;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * s = string.
+         */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(reverse(s));
 
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-
-	public static void main(final String[] args) {
-		/**
-		 * s = string.
-		 */
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		System.out.println(reverse(s));
-
-		}
-	}
+    }
+    }
