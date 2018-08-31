756933f1659ad808780ed1eb2fc1dd6ebf852a06
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-31 17:19:45 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -16,7 +16,7 @@ class Solution{
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
+		 int n = Integer.parseInt(sc.nextLine());
 		for(int i = 0 ;i<n;i++){
 			String s = sc.nextLine();
 			binarytodecimal(s);
