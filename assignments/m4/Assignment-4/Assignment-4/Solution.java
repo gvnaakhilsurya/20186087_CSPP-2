4bf2aa773f654d4fbb100d3bc1760cc8fe5ea6b4
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-31 14:24:38 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m4/Assignment-4/Assignment-4/Solution.java
+++ assignments/m4/Assignment-4/Assignment-4/Solution.java
@@ -1,6 +1,6 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
+/**
+ * @author :gvnaakhil
+ */
 import java.util.Scanner;
 /**
  * Class for solution.
