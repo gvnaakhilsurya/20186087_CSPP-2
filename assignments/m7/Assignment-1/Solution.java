1dafc53da34706096b11c1e3886c15b23c1b05ae
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-03 19:30:45 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m7/Assignment-1/Solution.java
+++ assignments/m7/Assignment-1/Solution.java
@@ -1,45 +1,44 @@
 import java.util.*;
 class InputValidator
 {
-	/**
-	 * It is attributes ddeclaration.
-	 */
-	int length;
-	String input;
-	InputValidator(String input)
-	{
-		/**
-		 * It is counstructor.
-		 */
-		this.input = input;
-		
-	}
-	boolean validateData()
-	{
-		/**
-		 * It is the conditional method.
-		 */
-		if (input.length()>=6){
-			return true;	
-		}else{
-			return false;
-		}
-	}
+    /**
+     * It is attributes ddeclaration.
+     */
+    int length;
+    String input;
+    InputValidator(String input)
+    {
+        /**
+         * It is counstructor.
+         */
+        this.input = input;
+    }
+    boolean validateData()
+    {
+        /**
+         * It is the conditional method.
+         */
+        if (input.length() >= 6){
+            return true;    
+        }else{
+            return false;
+        }
+    }
 }
 public class Solution
 {
-	/**
-	 * It is the main class.
-	 */
-	public static void main(String args[])
+    /**
+     * It is the main class.
+     */
+    public static void main(String args[])
     {
-    	/**
-    	 * It is the main class description.
-    	 */
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
-    	System.out.println(i.validateData());
+        /**
+         * It is the main class description.
+         */
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
     }
 
