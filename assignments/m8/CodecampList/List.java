295d2ca822981a7d5227cd99af2a2ed1389437c9
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-04 17:58:56 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- assignments/m8/CodecampList/List.java
+++ assignments/m8/CodecampList/List.java
@@ -323,6 +323,7 @@ public class List {
                 case "contains":
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
+                default:
             }
         }
     }
