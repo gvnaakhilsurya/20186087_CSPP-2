76529bed8d4430b57b52edbf019b340324de1e78
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-04 17:21:29 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- assignments/m8/CodecampList/List.java
+++ assignments/m8/CodecampList/List.java
@@ -160,7 +160,7 @@ public class List {
          * It get the elemnt in the list by giving the index value.
          */
         
-        for(int i = 0; i<this.size; i++) {
+        for (int i = 0; i<this.size; i++) {
             if (this.a[index] == this.a[i])
                 return this.a[i];
         }
@@ -192,7 +192,7 @@ public class List {
          */
         
         String str = "[";
-        for(int i = 0; i <this.size - 1; i++) {
+        for (int i = 0; i<this.size - 1; i++) {
             str += a[i] + ",";
         }
         return str + a[this.size - 1] + "]";
