cb033a7addd625f6ee3b2d3658105032efb1f435
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-05 10:33:03 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- assignments/m9/Assignment-1/Fibonacci.java
+++ assignments/m9/Assignment-1/Fibonacci.java
@@ -35,15 +35,16 @@ public class Fibonacci
             fibo.add(1);
         }else {
             fibo.add(0);
-        fibo.add(1);
-        int n1 = 1;
-        int n2 = 1;
-        int fibno = 1;
-        for(int i = 3;i<=n;i++) {
-            fibno = n1 + n2;
-            n1 = n2;
-            n2 = fibno;
-            fibo.add(fibno);
+            fibo.add(1);
+            fibo.add(1);
+            int n1 = 1;
+            int n2 = 1;
+            int fibno = 1;
+            for(int i = 3;i<=n;i++) {
+                fibno = n1 + n2;
+                n1 = n2;
+                n2 = fibno;
+                fibo.add(fibno);
         }
 
         }return fibo;
