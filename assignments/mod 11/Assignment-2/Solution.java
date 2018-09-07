fbe1122b5a2c701447b56b4ecb575029316a8664
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-07 19:53:22 +0530

    "check_style_score -> 7 of 8 passed. style score: 0/1 "

--- assignments/mod 11/Assignment-2/Solution.java
+++ assignments/mod 11/Assignment-2/Solution.java
@@ -505,14 +505,11 @@ public class Solution {
                     }
                 break;
                 case "subList": {
-                    if (tokens.length != 2) 
-                        break; 
+                    if (tokens.length != 2) break;
                     String[] arrstring3 = tokens[1].split(",");
-                    List object = l.subList(Integer.parseInt(arrstring3[0]),
-                     Integer.parseInt(arrstring3[1]));
-                    if (object != null) {
+                    List object = l.subList(Integer.parseInt(arrstring3[0]), Integer.parseInt(arrstring3[1]));
+                    if (object != null) 
                         System.out.println(object);
-                    }
                     break;
                 }
                 case "equals":
