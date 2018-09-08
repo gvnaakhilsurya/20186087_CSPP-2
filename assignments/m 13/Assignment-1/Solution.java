ad4158ec1d3e1d9f0a78017d1171fb28260040a0
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-08 12:56:25 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ assignments/m 13/Assignment-1/Solution.java
@@ -0,0 +1,185 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashSet;
+/**
+ * Class for set.
+ * @author : 
+ */
+class Set {
+    //your code goes here...
+    //Good luck :-)
+    private int []set;
+    private int size;
+
+    public Set() {
+    	set = new int[20];
+    	size = 0;
+    }
+
+    public Set(int x) {
+    	set = new int[x];
+    }
+
+    public void add(final int item) {
+    	if(size == set.length) {
+    		resize();
+    	}
+    	for (int i = 0;i < set.length; i++) {
+    		if(set[i] != item) {
+    			set[size++] = item;
+    		}
+    	}
+
+    }
+    public int size() {
+    	return size;
+    }
+
+
+    private void resize() {
+    	set = Arrays.copyOf(set, 2 * size);
+    }
+
+    public String toString() {
+
+    	if (size == 0) {
+    		return " ";
+    	}
+    	String str = "{";
+    	for (int i = 0; i < size - 1; i++) {
+    		str = str + set[i] + ","; 
+    	} return str + set[size - 1] + "}";
+    }
+    
+    public int indexOf(final int item) {
+
+    for (int i = 0; i < size; i++) {
+        if (set[i] == item) {
+            return i;
+        }
+    }
+     return 0;
+    }
+
+    public boolean contains(final int item) {
+
+	    if (indexOf(item) == -1) {
+	        return false;
+	    }
+	    return true;
+    }
+    public void add(final int[] newArray) {
+
+	    for (int i = 0; i < newArray.length; i++) {
+	        add(newArray[i]);
+	    }
+}
+    // public Set intersection(int[] newArray) {
+
+    // 	if (toString().equals(newArray.toString())) {
+    // 		int x [newArray.length] =  Arrays.sort(newArray);
+    // 		Set<Integer> targetSet = new HashSet<Integer>(Arrays.asList(x));
+    // 	}
+    // 	return targetSet;
+    // }
+    // public Set retainAll(int[] newrray) {
+
+    // }
+
+}
+/**
+ * Solution class for code-eval.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * helper function to convert string input to int array.
+     *
+     * @param      s     { string input from test case file }
+     *
+     * @return     { int array from the given string }
+     */
+    public static int[] intArray(final String s) {
+        String input = s;
+        if (input.equals("[]")) {
+            return new int[0];
+        }
+        if (s.contains("[")) {
+            input = s.substring(1, s.length() - 1);
+        }
+        return Arrays.stream(input.split(","))
+                            .mapToInt(Integer::parseInt)
+                            .toArray();
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // instantiate this set
+        Set s = new Set();
+        // code to read the test cases input file
+        Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+        // check if there is one more line to process
+        while (stdin.hasNext()) {
+            // read the line
+            String line = stdin.nextLine();
+            // split the line using space
+            String[] tokens = line.split(" ");
+            // based on the list operation invoke the corresponding method
+            switch (tokens[0]) {
+                case "size":
+                System.out.println(s.size());
+                break;
+                case "contains":
+                System.out.println(s.contains(Integer.parseInt(tokens[1])));
+                break;
+                case "print":
+                System.out.println(s);
+                break;
+                case "add":
+                int[] intArray = intArray(tokens[1]);
+                if (intArray.length == 1) {
+                    s.add(intArray[0]);
+                } else {
+                    s.add(intArray);
+                }
+                break;
+                // case "intersection":
+                // s = new Set();
+                // Set t = new Set();
+                // intArray = intArray(tokens[1]);
+                // s.add(intArray);
+                // intArray = intArray(tokens[2]);
+                // t.add(intArray);
+                // System.out.println(s.intersection(t));
+                // break;
+                // case "retainAll":
+                // s = new Set();
+                // intArray = intArray(tokens[1]);
+                // s.add(intArray);
+                // intArray = intArray(tokens[2]);
+                // System.out.println(s.retainAll(intArray));
+                // break;
+                // case "cartesianProduct":
+                // s = new Set();
+                // t = new Set();
+                // intArray = intArray(tokens[1]);
+                // s.add(intArray);
+                // intArray = intArray(tokens[2]);
+                // t.add(intArray);
+                // System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
+                // break;
+                default:
+                break;
+            }
+        }
+    }
+}
