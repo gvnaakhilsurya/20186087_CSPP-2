b25906ab66e7e979cc3e5313929e2d53eefef797
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-16 18:55:32 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

---
+++ assignments/m16/bookyourshow/Data.java
@@ -0,0 +1,16 @@
+class Contacts {
+
+	private int[] contact;
+	private String nameofuser;
+	private String numberofuser;
+
+	public Contacts(int []contact,String nameofuser,String numberofuser) {
+		contact = new int[];
+		name = nameof user;
+		number = numberofuser;
+ 	}
+
+ 	public 
+
+
+}
