45f699b52e0c816240f1e54eac2298a8f0579e3b
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-17 10:06:23 +0530

    "check_style_score -> 1 of 13 passed. style score: 0/1 "

--- assignments/m20/Assignment-1/Solution.java
+++ assignments/m20/Assignment-1/Solution.java
@@ -236,9 +236,9 @@ public final class Solution {
         final Quiz quiz, final int q) {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
-         String s1 = scan.nextLine();
-         String []question = s1.split(":");
-         String []choices = question[1].split(",");
+         // String s1 = scan.nextLine();
+         // String []question = s1.split(":");
+         // String []choices = question[1].split(",");
          // if (choices.length <2) {
          //    System.out.println("trick question  does not have enough answer choices");  
          // }
