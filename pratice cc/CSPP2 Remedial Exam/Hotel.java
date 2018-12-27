63b87debce34926f5c220fd2f38633af4929ef6e
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-12-27 16:38:45 +0530

    "cspp2_m10_a1_01: 4/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- pratice cc/CSPP2 Remedial Exam/Hotel.java
+++ pratice cc/CSPP2 Remedial Exam/Hotel.java
@@ -4,41 +4,72 @@ public class Hotel{
 	Reservation[]rooms;
 	int size;
 
-
-
 	public Hotel(){
-		this.rooms = new Reservation[5];
-		this.size = 0;
+		rooms = new Reservation[6];
+		size = 1;
+	}
+	public int size(){
+		return size;
 	}
 	public int reserveRoom(String person){
-		if(this.size == rooms.length){
+		if (size == rooms.length) {
 			System.out.println("All Rooms are reserved");
 		}
-		int num = 1;
-		int i = 0;
-		for (i = 0;i < rooms.length-1;i++) {
-			rooms[num + i] = new Reservation(person, num+i);
+		int i = 1;
+		for ( i = 1;i <rooms.length;i++) {
+			if (rooms[i]== null) {
+				rooms[i] = new Reservation(person,i);
+				size++;
+				return i;
+			}
 		}
-		size++;
-		// System.out.println(size);
-		return size;
-		}
+		return -1;
 		
+	}
+	public boolean isEmpty(int roomNumber){
+		for (int i = 1;i < rooms.length;i++){
+			if (rooms[roomNumber]== null) {
+				return true;
+			}
+		}
+		return false;
+	}
 	public boolean reserveRoom(String person,int roomNumber){
-		if (rooms[roomNumber-1] != null) {
+		if (size == rooms.length) {
+			System.out.println("All Rooms are reserved");
 			return false;
-		}else{
-			// rooms[roomNumber-1] = new Reservation(person);
-			// size++;
-			// System.out.println(size);
+		}else if(!isEmpty(roomNumber)){
+			System.out.println("Room is already reserved");
+			return false;
+		} else {
+			rooms[roomNumber] = new Reservation(person,roomNumber);
+			size++;
+		}
 		return true;
 	}
-
-	}
-
 	public void printReservations(){
-		for (int i = 1;i < this.size ;i++) {
-			System.out.println(rooms[i]);
+		for (int i = 1;i < rooms.length;i++) {
+			if (rooms[i]!= null) {
+				System.out.println(rooms[i]);
+			}
 		}
 	}
+	public void cancelReservations(String person){
+		for (int i = 0;i < size ;i++) {
+			if (rooms[i]!= null) {
+				if (rooms[i].getpersonName().equals(person)) {
+					rooms[i] = null;
+				}
+			}
+			
+		}
+	}
+		public boolean buildRooms(int num) {
+			Reservation[] temp = new Reservation[rooms.length+num];
+			System.arraycopy(rooms,0,temp,0,rooms.length);
+			rooms = temp;
+			return true;
+			}
+
+
 }
