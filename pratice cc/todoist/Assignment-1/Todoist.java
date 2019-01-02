4f966455f5cf68adb0f4c8f0642a935e21b66a6c
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2019-01-02 13:25:22 +0530

    "final_exam_a1_1: 6/14 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- pratice cc/todoist/Assignment-1/Todoist.java
+++ pratice cc/todoist/Assignment-1/Todoist.java
@@ -1,19 +1,28 @@
 class Todoist{
 	List<Task> taskslist;
+	int size;
 
 	public Todoist(){
 		taskslist = new List<Task>();
+		size = taskslist.size();
 
 	}
 	public void addTask(Task task){
-
+		taskslist.add(task);
+		size++;
 	}
 
-	public Task getNextTask(String task){
+	public Task getNextTask(String task_title){
+		for (int i = 0;i < size;i++) {
+			if (task_title.equals(taskslist.get(i).getTitle())) {
+				return taskslist.get(i);
+			}
+		}
 		return null;
 	}
-	public Task[] getNextTask(String task,int num_tasks){
+	public Task[] getNextTask(String task_title,int tt_tasks){
 		return null;
+		
 	}
 	public int  totalTime4Completion(){
 		return 0;
