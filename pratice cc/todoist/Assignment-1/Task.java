4da8ad2635747376d9d953431949b34db4f9a67a
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2019-01-02 11:39:39 +0530

    "final_exam_a1_1: 0/14 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ pratice cc/todoist/Assignment-1/Task.java
@@ -0,0 +1,60 @@
+class Task{
+	String title;
+	String assignedTo;
+	int timeTocomplete;
+	boolean important;
+	boolean urgent;
+	String status;
+
+	public Task(String title, String assignedTo,int timeTocomplete, boolean important,
+		boolean urgent,String status){
+		this.title = title;
+		this.assignedTo = assignedTo;
+		this.timeTocomplete = timeTocomplete;
+		this.important = important;
+		this.urgent = urgent;
+		this.status = status;
+	}
+	 public String getTitle(){
+	  return title;
+	 }
+	 
+	 public void setTitle(String title){
+	  this.title = title;
+	 }
+	  public String getAssignedTo(){
+	   return assignedTo;
+	  }
+	  
+	  public void setAssignedTo(String assignedTo){
+	   this.assignedTo = assignedTo;
+	  }
+	   public int getTimeTocomplete(){
+	    return timeTocomplete;
+	   }
+	   
+	   public void setTimeTocomplete(int timeTocomplete){
+	    this.timeTocomplete = timeTocomplete;
+	   }
+	    public boolean getImportant(){
+	     return important;
+	    }
+	    
+	    public void setImportant(boolean important){
+	     this.important = important;
+	    }
+	     public boolean getUrgent(){
+	      return urgent;
+	     }
+	     
+	     public void setUrgent(boolean urgent){
+	      this.urgent = urgent;
+	     }
+
+	     public String toString(){
+	     	String str = "";
+	     	str = this.title + ", "+this.assignedTo + ", "+this.timeTocomplete+
+	     	", "+this.important+", "+this.urgent+", "+this.status;
+	     	return str;
+	     }
+}
---
+++ pratice cc/todoist/Assignment-1/Todoist.java
@@ -0,0 +1,24 @@
+class Todoist{
+	List<Task> taskslist;
+
+	public Todoist(){
+		taskslist = new List<Task>();
+
+	}
+	public void addTask(Task task){
+
+	}
+
+	public Task getNextTask(String task){
+		return null;
+	}
+	public Task[] getNextTask(String task,int num_tasks){
+		return null;
+	}
+	public int  totalTime4Completion(){
+		return 0;
+
+	}
+
+
+}
